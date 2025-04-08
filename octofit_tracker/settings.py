abd5a4474ab5cf70500948b3a3c3d12c3b7d7269
AkioForDev <137969704+AkioForDev@users.noreply.github.com>
2025-04-08 09:34:30 +0000

    Update settings.py to use djongo as the database engine

--- octofit_tracker/settings.py
+++ octofit_tracker/settings.py
@@ -73,9 +73,12 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'CLIENT': {
+            'host': 'mongodb://localhost:27017',
+        },
     }
 }
 
